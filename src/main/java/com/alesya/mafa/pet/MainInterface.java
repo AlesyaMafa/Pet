d6bcacc530e0f6d5f17150402267ec46820ad3a6
AlesyaMafa <alesya.ens.com>
2020-05-13 19:47:32 +0300

    Remove flag and extra 'if' loops.

--- src/main/java/com/alesya/mafa/pet/MainInterface.java
+++ src/main/java/com/alesya/mafa/pet/MainInterface.java
@@ -8,50 +8,37 @@ public class MainInterface implements IMainInterface {
     private final static String INCORRECT_VALUE = "Incorrect value, try again";
     private final static String INPUT_PET = "Input type of pet: cat, dog or horse";
     private final static String INPUT_APP = "Type 'add' to add new pet\nType 'update' to update existing pet\nType 'delete' to delete existing pet\nType 'list' to view all pets\nType 'exit' to stop app";
-    private final static String UPDATE_PET = "To updtae pet type ID\nIf you don't remember ID, type list";
+    private final static String UPDATE_PET = "To update pet type ID\nIf you don't remember ID, type list";
     private final static String DELETE_PET = "To delete pet type ID\nIf you don't remember ID, type list";
     private final static String DELETE_PET_BY_ID = "To delete pet type ID";
-    private final static String UPDATE_PET_BY_ID = "To updtae pet type ID";
+    private final static String UPDATE_PET_BY_ID = "To update pet type ID";
 
     private IOperationInterface operationInterface = new OperationInterface();
     private Scanner scanner = new Scanner(System.in);
-    private String input;
 
     @Override
     public void run() {
-
-        boolean flag;
         operationInterface.setScanner(scanner);
-
+        exit:
         while (true) {
-
-            flag = false;
-
             System.out.println(INPUT_APP);
-            input = scanner.nextLine().trim();
-            if (input.equals("exit")) {
-                break;
-            }
-            if (input.equals("list")) {
-                operationInterface.listPet();
-                flag = true;
-            }
-
-            if (input.equals("add")) {
-                add();
-                flag = true;
-            }
-            if (input.equals("update")) {
-                update();
-                flag = true;
-            }
-            if (input.equals("delete")) {
-                delete();
-                flag = true;
-            }
-
-            if (!flag) {
-                System.out.println(INCORRECT_VALUE);
+            switch (scanner.nextLine().trim()) {
+                case "exit":
+                    break exit;
+                case "list":
+                    operationInterface.listPet();
+                    break;
+                case "add":
+                    add();
+                    break;
+                case "update":
+                    update();
+                    break;
+                case "delete":
+                    delete();
+                    break;
+                default:
+                    System.out.println(INCORRECT_VALUE);
             }
         }
         scanner.close();
@@ -60,20 +47,19 @@ public class MainInterface implements IMainInterface {
     private void add() {
         while (true) {
             System.out.println(INPUT_PET);
-            input = scanner.nextLine().trim();
-            if (input.equals("cat")) {
-                operationInterface.addCat();
-                return;
+            switch (scanner.nextLine().trim()) {
+                case "cat":
+                    operationInterface.addCat();
+                    return;
+                case "dog":
+                    operationInterface.addDog();
+                    return;
+                case "horse":
+                    operationInterface.addHorse();
+                    return;
+                default:
+                    System.out.println(INCORRECT_VALUE);
             }
-            if (input.equals("dog")) {
-                operationInterface.addDog();
-                return;
-            }
-            if (input.equals("horse")) {
-                operationInterface.addHorse();
-                return;
-            }
-            System.out.println(INCORRECT_VALUE);
         }
     }
 
@@ -88,15 +74,14 @@ public class MainInterface implements IMainInterface {
             }
             if (Objects.isNull(operationInterface.deletePet(deletedId))) {
                 System.out.println(INCORRECT_VALUE);
-            }
-            else return;
+            } else return;
         }
     }
 
     private void update() {
         while (true) {
             System.out.println(UPDATE_PET);
-            String updateId= scanner.nextLine().trim();
+            String updateId = scanner.nextLine().trim();
             if (updateId.equals("list")) {
                 operationInterface.listPet();
                 System.out.println(UPDATE_PET_BY_ID);
@@ -104,8 +89,7 @@ public class MainInterface implements IMainInterface {
             }
             if (Objects.isNull(operationInterface.updatePet(updateId))) {
                 System.out.println(INCORRECT_VALUE);
-            }
-            else return;
+            } else return;
         }
     }
 }
